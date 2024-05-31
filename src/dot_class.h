947c623b2069b4c919f99ae40f905b723d92e72c
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-05-31 15:06:30 -0300

    leitura completa do constant pool

--- src/dot_class.h
+++ src/dot_class.h
@@ -12,27 +12,76 @@ typedef struct
   {
 
     // Tag: 7
-    struct
-    {
-      u2_t name_index;
-    } Class;
+    struct { u2_t name_index; } class;
 
-    // Tag: 7
+    // Tag: 9
     struct
     {
       u2_t class_index;
       u2_t name_and_type_index;
-    } Fieldref;
+    } fieldref;
 
     // Tag: 10
     struct
     {
       u2_t class_index;
       u2_t name_and_type_index;
-    } Methodref;
+    } methodref;
+
+    // tag: 11
+    struct
+    {
+      u2_t class_index;
+      u2_t name_and_type_index;
+    } interface_methodref;
+
+    // Tag: 8
+    struct
+    {
+      u2_t string_index;
+    } string;
+
+    // Tag: 3
+    struct
+    {
+      u4_t bytes;
+    } integer;
+
+    // Tag: 4
+    struct
+    {
+      u4_t bytes;
+    } float_num;
+
+    // Tag: 5
+    struct
+    {
+      u4_t high_bytes;
+      u4_t low_bytes;
+    } long_num;
+
+    // Tag: 6
+    struct
+    {
+      u4_t high_bytes;
+      u4_t low_bytes;
+    } double_num;
+
+    // Tag 12
+    struct
+    {
+      u2_t name_index;
+      u2_t descriptor_index;
+    } name_and_type;
+
+    // Tag: 1
+    struct
+    {
+      u2_t length_of_byte_array;
+      u1_t *string;
+    } utf8;
 
   } info;
-
 } cp_info_t;
 
 typedef struct
@@ -41,5 +90,5 @@ typedef struct
   u2_t minor_version;
   u2_t major_version;
   u2_t constant_pool_count;
-  cp_info_t constant_pool[];
+  cp_info_t * constant_pool;
 } class_file_t;
