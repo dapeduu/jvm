55a4509afd710dabeb4f88a1d9cac831e8abea3c
mendesmcmg <claraplanejamento@gmail.com>
2024-06-16 16:51:48 -0300

    feat: adds method_area types

--- src/runtime_data_area/method_area.h
+++ src/runtime_data_area/method_area.h
@@ -1,5 +1,12 @@
+#include "./core/core.h"
+#include "./core/class.h"
 
 typedef struct {
-  
+  class_t* classe;
+  hash_table_t* fields_table;
+} object_t;
 
+typedef struct {
+  hash_table_t loaded_classes;
+  object_t* objects;
 } method_area_t;
