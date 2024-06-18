a5ad47a3995e283f3df6425bb917cbf7d5a42de4
mendesmcmg <claraplanejamento@gmail.com>
2024-06-18 20:49:28 -0300

    refactor: remove unnecessary line

--- src/utils/hash_table_functions.c
+++ src/utils/hash_table_functions.c
@@ -5,8 +5,6 @@ hash_table_t* newHashTable(short table_size) {
   hash_table->table_size = table_size;
   hash_table->data_size = 0;
   hash_table->entries = calloc(hash_table->table_size, sizeof(key_value_t*));
-  for (int i = 0; i < table_size; i++) {
-    hash_table->entries[i] = NULL;
-  }
+  
   return hash_table;
 }
