43b0edba5d5a85b533e5ba7a516ad506856dc044
Clara Mendes <claraplanejamento@gmail.com>
2024-06-25 22:37:12 -0300

    reduce negates

--- test/Negate.java
+++ test/Negate.java
@@ -3,24 +3,24 @@ public class Negate {
         return -value;
     }
 
-    public long negateLong(long value) {
-        return -value;
-    }
+    // public long negateLong(long value) {
+    //     return -value;
+    // }
 
-    public float negateFloat(float value) {
-        return -value;
-    }
+    // public float negateFloat(float value) {
+    //     return -value;
+    // }
 
-    public double negateDouble(double value) {
-        return -value;
-    }
+    // public double negateDouble(double value) {
+    //     return -value;
+    // }
 
     public static void main(String[] args) {
         Negate test = new Negate();
 
         System.out.println("Negate int 5: " + test.negateInt(5));
-        System.out.println("Negate long 10L: " + test.negateLong(10L));
-        System.out.println("Negate float 5.5f: " + test.negateFloat(5.5f));
-        System.out.println("Negate double 10.5: " + test.negateDouble(10.5));
+        // System.out.println("Negate long 10L: " + test.negateLong(10L));
+        // System.out.println("Negate float 5.5f: " + test.negateFloat(5.5f));
+        // System.out.println("Negate double 10.5: " + test.negateDouble(10.5));
     }
 }
