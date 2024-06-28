d343e45624ca6023ea7a01578850e78f4469a613
mendesmcmg <claraplanejamento@gmail.com>
2024-06-28 20:29:48 -0300

    doxygen generate

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -5,5 +5,6 @@ var searchData=
   ['read_5fu2_2',['read_u2',['../d3/db7/class__loader_8c.html#ad707bafcf4fec166f8e995e60675d504',1,'class_loader.c']]],
   ['read_5fu4_3',['read_u4',['../d3/db7/class__loader_8c.html#ac70609e32c77ce3dc8c96ffa390697ac',1,'class_loader.c']]],
   ['rodar_4',['rodar',['../index.html#autotoc_md6',1,'Como rodar'],['../d3/dcc/md__r_e_a_d_m_e.html#autotoc_md10',1,'Como rodar']]],
-  ['run_5fframe_5',['run_frame',['../d0/d6d/execution__engine_8c.html#a9ca98370dd722a6196a7ee8104459224',1,'execution_engine.c']]]
+  ['run_5fframe_5',['run_frame',['../d0/d6d/execution__engine_8c.html#a9ca98370dd722a6196a7ee8104459224',1,'execution_engine.c']]],
+  ['runtime_5fdata_5farea_2ec_6',['runtime_data_area.c',['../d4/d25/runtime__data__area_8c.html',1,'']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['get_5fconstant_5fpool_5fvalue_0',['get_constant_pool_value',['../d3/db7/class__loader_8c.html#a3e07a39b119a061031dd7e3304ce30d5',1,'class_loader.c']]]
+  ['get_5fconstant_5fpool_5fvalue_0',['get_constant_pool_value',['../d3/db7/class__loader_8c.html#a3e07a39b119a061031dd7e3304ce30d5',1,'class_loader.c']]],
+  ['get_5fframe_1',['get_frame',['../d4/d25/runtime__data__area_8c.html#a59acc909558e827494cf19f94442e07d',1,'runtime_data_area.c']]]
 ];
