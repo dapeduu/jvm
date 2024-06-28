6f45c575d9a7d0cc408dff6a2f886f88fda371f8
mendesmcmg <claraplanejamento@gmail.com>
2024-06-28 19:50:27 -0300

    run_frame

--- src/execution_engine.c
+++ src/execution_engine.c
@@ -1,3 +1,10 @@
+/**
+ * @file execution_engine.c
+ * @brief Implementação das funções de execução.
+ * Declara e usa funções da pilha de frames.
+ * Roda o frame chamando uma instrução por vez.
+ */
+
 #include <stdlib.h>
 #include "core.h"
 #include "handlers.h"
@@ -5,18 +12,43 @@
 #include "execution_engine.h"
 
 // Operação de push no frame (colocar um argumento na pilha)
+/**
+ * @brief Função push.
+ *
+ * Coloca um argumento na pilha.
+ *
+ * @param[in]  frame  Ponteiro para o frame.
+ * @param[in] argument  Argumento a ser colocado na pilha.
+ * @return void.
+ */
+
 void push(frame_t * frame, u4_t argument) {
     frame->stack[frame->stack_count] = argument;
     frame->stack_count += 1;
 }
 
 // Operação de pop no frame (retirar o último argumento da pilha)
+/**
+ * @brief Função pop.
+ *
+ * Retira o último argumento da pilha.
+ *
+ * @param[in]  frame  Ponteiro para o frame.
+ * @return Último argumento da pilha.
+ */
+
 u4_t pop(frame_t * frame) {
     frame->stack_count -= 1;
     return frame->stack[frame->stack_count];
 }
 
-// Roda o frame, chamando uma instrução por vez
+/**
+ * @brief Função run_frame.
+ * 
+ * Roda o frame, chamando uma instrução por vez.
+ * @param frame 
+ * @return u4_t 
+ */
 u4_t run_frame(frame_t * frame) {
     for (size_t i = 0; i < frame->code_length; i++)
     {
